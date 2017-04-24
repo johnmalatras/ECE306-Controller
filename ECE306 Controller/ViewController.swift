380c32e2f7170f55f11ea8154258c32ce3b1dcee
John Malatras <john.malatras@gmail.com>
2017-04-24 15:48:35 -0400

    Initial Commit

--- ECE306 Controller/ViewController.swift
+++ ECE306 Controller/ViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
-
+    
+    var ipAddress = "172.20.10.7"
+    @IBOutlet weak var IPTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func ForwardButton(_ sender: Any) {
+        Alamofire.request("http://" + ipAddress + "/" + "output.cgi?text=.1212F1&submit=Submit")
+    }
 
+    @IBAction func RightButton(_ sender: Any) {
+        Alamofire.request("http://" + ipAddress + "/" + "output.cgi?text=.1212R9&submit=Submit")
+    }
+    
+    @IBAction func LeftButton(_ sender: Any) {
+        Alamofire.request("http://" + ipAddress + "/" + "output.cgi?text=.1212L9&submit=Submit")
+    }
+    
+    @IBAction func ReverseButton(_ sender: Any) {
+        Alamofire.request("http://" + ipAddress + "/" + "output.cgi?text=.1212B9&submit=Submit")
+    }
+    
+    @IBAction func StopButton(_ sender: Any) {
+        Alamofire.request("http://" + ipAddress + "/" + "output.cgi?text=.1212S1&submit=Submit")
+    }
+    
+    @IBAction func CircleEndButton(_ sender: Any) {
+        Alamofire.request("http://" + ipAddress + "/" + "output.cgi?text=.1212C1&submit=Submit")
+    }
+    
+    @IBAction func SaveIPButton(_ sender: Any) {
+        self.ipAddress = self.IPTextField.text!
+    }
+    
+    //Calls this function when the tap is recognized.
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
 }
 
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'ECE306 Controller' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ECE306 Controller
+  pod 'Alamofire', '~> 4.4'
+
+end
