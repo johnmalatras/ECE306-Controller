9fd0817acd2026a18bf73ba99bbed6fe8d398cda
John Malatras <john.malatras@gmail.com>
2017-04-24 13:06:01 -0400

    Initial Commit

---
+++ ECE306 Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ECE306 Controller
+//
+//  Created by John Malatras on 4/24/17.
+//  Copyright © 2017 John Malatras. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
